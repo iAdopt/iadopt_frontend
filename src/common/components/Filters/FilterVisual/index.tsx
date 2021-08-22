9d31e04db4447fba45915c33f7ed53498dab1b7b
Cristina Bonamusa <cristinabonamusa@gmail.com>
2021-08-22 13:26:01 +0200

    iAdopt Frontend state Visual boolean v01 v02

--- src/common/components/Filters/FilterVisual/index.tsx
+++ src/common/components/Filters/FilterVisual/index.tsx
@@ -2,26 +2,39 @@ import styles from './Styles.module.scss';
 import Image from 'next/image'
 import v01 from '../../../../../public/v01.png'
 import v02 from '../../../../../public/v02.png'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 
 export const VisualFilter = ({
 ...props
 }) => { 
-const [stateVisual, setStateVisual] = useState({ v01: true, v02:false});
-const handleClick = (e:any) => {
-    // setStateVisual((prevState:boolean) => ({}));
+const [stateVisual, setStateVisual] = useState({ v01:true, v02:false});
+const buttonFocused = useRef(null);
+
+useEffect(() => {
+    buttonFocused.current.focus();
+}, []);
+
+
+const handleClickv01 = (event:any) => {
+    setStateVisual(() => ({v01:true, v02:false}));
 }
+
+const handleClickv02 = (event:any) => {
+    setStateVisual(() => ({v01:false, v02:true})); 
+}
+
 return(
     <div className={styles.container}>
         <p className={styles.result}><strong>Resultado:</strong></p>
         <div className={styles.visualBox}>
             <p><strong>Visualización:</strong></p>
-            <button onClick={handleClick}>
-                <Image src={v01} width="56" alt="visualization01"/>
+
+            <button className={stateVisual.v01  ? 'activeClass' : ''} onClick={handleClickv01} ref={buttonFocused}>
+                <Image id="v01" src={v01} width="56" alt="visualization01"/>
             </button>
-            <button autoFocus={stateVisual.v02}onClick={handleClick}>
-                <Image src={v02} width="56" alt="visualization02"/>
+            <button className={stateVisual.v02  ? 'activeClass' : ''} onClick={handleClickv02}>
+                <Image id="v02" src={v02} width="56" alt="visualization02"/>
             </button>
         </div>     
     </div>

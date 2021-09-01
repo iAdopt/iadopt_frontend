e20b6e1a55f90442de2b9c74b66c6bff36e4d45a
Cristina Bonamusa <cristinabonamusa@gmail.com>
2021-09-01 21:45:16 +0200

    iAdopt FE - form add animal opt

--- src/common/components/Admin/AdminAddAnimalForm.tsx
+++ src/common/components/Admin/AdminAddAnimalForm.tsx
@@ -14,40 +14,28 @@ export const AdminAddAnimalForm = (props:any) => {
         vaccinated: "",
         sterilized: "",
         identified: "",
-        image: ""
+        otherissues: "",
+        image: "",
+        status: ""
     })
     const form = useRef(null);
 
-    // const submitForm = (event) => {
-    //    event.preventDefault();
-    //    const data = new FormData(form.current)
-    //    fetch('', {
-    //        method: 'POST',
-    //        body: data
-    //    })
-    //    .then(response => response.json())
-    //    .then(json => setAnimalState(json.animal))
-    // }
-
-    const submitForm = (event) => {
+    const submitForm = async (event:any) => {
         event.preventDefault();
-        // try {
-        //     const data = animal
-        //     await fetch('/api', {
-        //         method: 'POST',
-        //         mode: 'no-cors',
-        //         headers: {
-        //             'Accept' : 'application/json',
-        //             'Content-type': 'application/json'
-        //         },
-        //         body: data
-        //     });
-        //     console.log('result', data)
+        try {
+            await fetch('/api', {
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Accept' : 'application/json',
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(animal)
+            });
 
-        // } catch(error) {
-        //     console.error(error)
-        // }
-        console.log(animal)
+        } catch(error) {
+            console.error('Error to submit information:', error)
+        }
      }
 
     const handleChange = (e:any) => {
@@ -56,12 +44,12 @@ export const AdminAddAnimalForm = (props:any) => {
             [e.target.name]: e.target.value
         })
     }
+
     return(
         <div className={styles.adminAddAnimal}>
             <h3>Añadir un animal al centro</h3>
 
             <form  className={styles.form} ref={form} onSubmit={submitForm}>
-
                 <h4>Información general</h4>
                 <div className={styles.block}>
                     <div className={styles.inputs}>
@@ -79,14 +67,14 @@ export const AdminAddAnimalForm = (props:any) => {
                     </div>
 
                     <div className={styles.inputs}>
-                        <label htmlFor="animal[age]">Fecha de nacimiento:</label>
-                        <input type="date" name="animal[age]" placeholder="Nombre" value={animal.age}/>
+                        <label htmlFor="age">Fecha de nacimiento:</label>
+                        <input type="date" name="age" placeholder="Nombre" value={animal.age} onChange={handleChange}/>
                         <p>Si se desconoce fecha de nacimiento del animal, selecciona una fecha aproximada</p>
                     </div>
 
                     <div className={styles.inputs}>
-                        <label htmlFor="animal[gender]">Genero:</label>
-                        <select name="animal[gender]" value={animal.gender} >
+                        <label htmlFor="gender">Genero:</label>
+                        <select name="gender" value={animal.gender} onChange={handleChange}>
                             <option value="0">Seleciona el género del animal</option>
                             <option value="female">Femenino</option>
                             <option value="male">Masculino</option>
@@ -94,8 +82,8 @@ export const AdminAddAnimalForm = (props:any) => {
                     </div>
 
                     <div className={styles.inputs}>
-                        <label htmlFor="animal[description]">Descripción:</label>
-                        <textarea name="animal[description]" placeholder="Descripción del animal" value={animal.description}/>
+                        <label htmlFor="description">Descripción:</label>
+                        <textarea name="description" placeholder="Descripción del animal" value={animal.description}  onChange={handleChange}/>
                     </div>
                 </div>
 
@@ -104,12 +92,18 @@ export const AdminAddAnimalForm = (props:any) => {
                     <h5>Vacunación:</h5>
                     <div className={styles.radios}>
                         <div>
-                            <input type="radio" name="noVaccinated" value={animal.vaccinated} onChange={e => {setAnimalState(prev => ({...prev, [prev.vaccinated]: false}))}}/>
+                            <input type="radio" name="noVaccinated" value='0' id="vaccinated" 
+                                onChange={(e:any) => { setAnimalState(prev => ({...animal, [e.target.id]:e.target.value}))}}
+                                checked={animal.vaccinated == '0' ? true : false} 
+                            />
                             <label htmlFor="noVaccinated">Sin vacunación</label>
                         </div>
 
                         <div>
-                            <input type="radio" name="completed" value={animal.vaccinated} onChange={e => {setAnimalState(prev => ({...prev, [prev.vaccinated]: true}))}}/>
+                            <input type="radio" name="vaccinated" value='1' 
+                                onChange={(e:any) => { setAnimalState(animal => ({...animal, [e.target.name]:e.target.value}))}}
+                                checked={animal.vaccinated == '1' ? true : false} 
+                             />
                             <label htmlFor="completed">Vacunación completa</label>
                         </div>
                     </div>
@@ -117,11 +111,17 @@ export const AdminAddAnimalForm = (props:any) => {
                     <h5>Esterilización:</h5>
                     <div className={styles.radios}>
                         <div>
-                            <input type="radio" name="sterilized" />
+                            <input type="radio" name="sterilized" value='1'
+                                onChange={(e:any) => { setAnimalState(animal => ({...animal, [e.target.name]:e.target.value}))}}
+                                checked={animal.sterilized == '1' ? true : false} 
+                            />
                             <label htmlFor="sterilized">Si</label>
                         </div>
                         <div>
-                            <input type="radio" name="noSterilized" />
+                            <input type="radio" name="noSterilized" id="sterilized" value='0'
+                                onChange={(e:any) => { setAnimalState(prev => ({...animal, [e.target.id]:e.target.value}))}}
+                                checked={animal.sterilized == '0' ? true : false} 
+                            />
                             <label htmlFor="noSterilized">No</label>
                         </div>
                     </div>
@@ -129,19 +129,25 @@ export const AdminAddAnimalForm = (props:any) => {
                     <h5>Microchip identificativo:</h5>
                     <div className={styles.radios}>
                         <div>
-                            <input type="radio" name="identified" />
+                            <input type="radio" name="identified"  value='1'
+                                onChange={(e:any) => { setAnimalState(animal => ({...animal, [e.target.name]:e.target.value}))}}
+                                checked={animal.identified == '1' ? true : false} 
+                            />
                             <label htmlFor="identified">Si</label>
                         </div>
 
                         <div>
-                            <input type="radio" name="noIdentified" />
+                            <input type="radio" name="noIdentified" id="identified" value='0' 
+                                onChange={(e:any) => { setAnimalState(prev => ({...animal, [e.target.id]:e.target.value}))}}
+                                checked={animal.identified == '0' ? true : false} 
+                            />
                             <label htmlFor="noIdentified">No</label>
                         </div>
                     </div>
 
                     <div className={styles.textZone}>
                         <label htmlFor="medicalDescription">Otras patologias:</label>
-                        <textarea name="medicalDescription" placeholder="Descripción del animal"/>
+                        <textarea name="medicalDescription" placeholder="Descripción de otras patologías del animal" value={animal.otherissues} onChange={handleChange}/>
                     </div>
                 </div>
 
@@ -149,10 +155,21 @@ export const AdminAddAnimalForm = (props:any) => {
                 <div className={styles.block}>
                     <div className={styles.inputs}>
                         <label htmlFor="image">Imágen del animal:</label>
-                        <input type="file" name="image" />
+                        <input type="file" name="image" onChange={handleChange} />
                     </div>
                 </div>
 
+                <h4>Relevancia</h4>
+                <div className={styles.block}>
+                    <div className={styles.inputs}>
+                        <label htmlFor="status">Status en el centro:</label>
+                        <select name="status" value={animal.status} onChange={handleChange}>
+                            <option value="">Seleciona la relevancia</option>
+                            <option value="new">Nuevo</option>
+                            <option value="urgent">Urgente</option>
+                        </select>
+                    </div>
+                </div>
 
                 <input type="submit" value="Añadir animal"></input>
             </form>
